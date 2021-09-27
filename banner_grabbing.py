894421c29b6f225e13f52e0a2943dcd40c5d14c9
Rafael B <77108325+rafaelbaldasso@users.noreply.github.com>
2021-09-27 16:30:35 -0300

    Update banner_grabbing.py

--- banner_grabbing.py
+++ banner_grabbing.py
@@ -3,11 +3,11 @@
 import socket,sys
 
 if len(sys.argv) <= 2:
-        print("\n [#] Banner Grabbing ~ by Kothmun\n")
+        print("\n [#] Banner Grabbing\n")
         print(" [>] Usage: ./banner_grabbing.py HOST PORT")
         print(" [>] Example: ./banner_grabbing.py 172.16.1.100 21\n")
 else:
-        print("\n [#] Banner Grabing ~ by Kothmun\n")
+        print("\n [#] Banner Grabing\n")
         print(" [>] Target: {}:{}\n".format(sys.argv[1],sys.argv[2]))
         mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         if mysocket.connect_ex((sys.argv[1],int(sys.argv[2]))) != 0:
