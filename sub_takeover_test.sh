989e56194c3c669272bfa36ad4c059aa3abb0730
Rafael B <77108325+rafaelbaldasso@users.noreply.github.com>
2021-09-27 16:38:43 -0300

    Update sub_takeover_test.sh

--- sub_takeover_test.sh
+++ sub_takeover_test.sh
@@ -3,14 +3,14 @@
 if [ "$1" == "" ] || [ "$2" == "" ]
 then
 	echo ""
-	echo " [#] Subdomain Takeover Test ~ by Kothmun"
+	echo " [#] Subdomain Takeover Test"
 	echo ""
 	echo " [>] Usage: $0 SITE WORDLIST"
 	echo " [>] Example: $0 site.com list.txt"
 	echo ""
 else	
 	echo ""
-	echo " [#] Subdomain Takeover Test ~ by Kothmun"
+	echo " [#] Subdomain Takeover Test"
 	echo ""
 	for word in $(cat $2);
 	do
