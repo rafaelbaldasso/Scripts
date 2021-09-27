80d425318477f138a623c9cb50adebbe44192b2d
Rafael B <77108325+rafaelbaldasso@users.noreply.github.com>
2021-09-27 16:31:26 -0300

    Update dns_zone_transfer.sh

--- dns_zone_transfer.sh
+++ dns_zone_transfer.sh
@@ -3,14 +3,14 @@
 if [ "$1" == "" ]
 then
 	echo ""
-	echo " [#] DNS Zone Transfer ~ by Kothmun"
+	echo " [#] DNS Zone Transfer"
 	echo ""
 	echo " [>] Usage: $0 HOST"
 	echo " [>] Example: $0 site.com"
 	echo ""
 else
 	echo ""
-	echo " [#] DNS Zone Transfer ~ by Kothmun"
+	echo " [#] DNS Zone Transfer"
 	echo ""
 	
 	for server in $(host -t ns $1 | cut -d " " -f 4);
