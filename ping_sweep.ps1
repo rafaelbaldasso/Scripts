1e2fd248516fd589da0f8a8c50d743297089b856
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2024-11-11 11:45:36 -0300

    Update ping_sweep.ps1

--- ping_sweep.ps1
+++ ping_sweep.ps1
@@ -1,23 +1,17 @@
 param($net)
 if (!$net) {
-    echo ""
-    echo " [#] Ping Sweep"
-	echo ""
-    echo " [>] Usage: .\ping_sweep.ps1 NETWORK"
-    echo " [>] Example: .\ping_sweep.ps1 172.16.1"
-    echo ""
+    Write-Output "`n [#] Ping Sweep"
+    Write-Output "`n [>] Usage: .\ping_sweep.ps1 NETWORK"
+    Write-Output " [>] Example: .\ping_sweep.ps1 172.16.1`n"
 } else {
-    echo ""
-    echo " [#] Ping Sweep"
-    echo ""
-    echo " [>] Network: $net.X"
-    echo ""
-    echo " [>] IPs:"
+    Write-Output "`n [#] Ping Sweep"
+    Write-Output "`n [>] Network: $net.0"
+    Write-Output "`n [>] IPs:"
 foreach ($ip in 1..254) {
 try {$resp = ping -n 1 -w 300 "$net.$ip" | Select-String "bytes=32"
 $ips = $resp.Line.split(' ')[2] -replace ":",""
 echo " [+] $ips"
 } catch {}
 }
-echo ""
+Write-Output ""
 }
